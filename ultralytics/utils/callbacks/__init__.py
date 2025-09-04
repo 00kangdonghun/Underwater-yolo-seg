e4cb89d40a433c1964d6b88b94a1e6ca6e581739
Donghun Kang <112920967+00kangdonghun@users.noreply.github.com>
2025-09-04 11:45:28 +0900

    Update __init__.py

--- ultralytics/utils/callbacks/__init__.py
+++ ultralytics/utils/callbacks/__init__.py
@@ -1,5 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
 from .base import add_integration_callbacks, default_callbacks, get_default_callbacks
+from .callbacks_uw import on_preprocess_batch
 
-__all__ = 'add_integration_callbacks', 'default_callbacks', 'get_default_callbacks'
+__all__ = 'add_integration_callbacks', 'default_callbacks', 'get_default_callbacks', 'on_preprocess_batch'
