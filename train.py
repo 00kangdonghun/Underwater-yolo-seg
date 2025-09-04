c31342eb9720b21b6e9ad17cd4c24c1a39bd4a9a
Donghun Kang <112920967+00kangdonghun@users.noreply.github.com>
2025-09-04 11:37:54 +0900

    Update train.py

--- train.py
+++ train.py
@@ -3,14 +3,36 @@ import os
 os.environ['WANDB_DISABLED'] = 'true'
 from ultralytics import YOLO
 
-model =YOLO("snn_yolov8l.yaml").load('69M_best.pt')
+# <model>
+
+# model =YOLO("/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/models/v8/yolov8-seg.yaml").load("/home1/danny472/Underwater/SpikeYOLO/yolov8n-seg.pt")
+
+# model =YOLO("/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/models/v8/yolov8-seg.yaml")
+
+model = YOLO("/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/models/v8/uw-final.yaml")
 
 print(model)
 
-#train
-# model.train(data="coco.yaml",device=[7],epochs=100)  # train the model
-model.train(data="coco.yaml",device=[4],epochs=100)  # train the model
+# <train>
 
-#TEST
-# model = YOLO('runs/detect/train1/weights/last.pt')  # load a pretrained model (recommended for training)
+#############################################################################################
+import sys
+sys.path.append("/home1/danny472/Underwater/SpikeYOLO/ultralytics/utils/callbacks")  
+from callbacks_uw import on_preprocess_batch
+model.add_callback("on_preprocess_batch", on_preprocess_batch)
+#############################################################################################
 
+# <dataset>
+model.train(data="/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/datasets/UIIS10K.yaml", device="0,1,2,3", epochs=100, project="/home1/danny472/Underwater/SpikeYOLO/runs")  # train the model
+# model.train(data="/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/datasets/deposition_302.yaml", device="0,1,2,3", epochs=100, project="/home1/danny472/Underwater/SpikeYOLO/runs")  # train the model
+# model.train(data="/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/datasets/marine_262.yaml", device="0,1,2,3", epochs=100, project="/home1/danny472/Underwater/SpikeYOLO/runs")  # train the model
+# model.train(data="/home1/danny472/Underwater/SpikeYOLO/ultralytics/cfg/datasets/trash_295.yaml", device="0,1,2,3", epochs=100, project="/home1/danny472/Underwater/SpikeYOLO/runs")  # train the model
+
+
+# python train.py > ./STDOUT/${LOG_PREFIX}.out 2> ./STDERR/${LOG_PREFIX}.err
+
+# <TEST>
+# model = YOLO('/home1/danny472/Underwater/SpikeYOLO/runs/UIIS10K/uw-final/train/weights/best.pt')  # load a pretrained model (recommended for training)
+
+# <predict>
+# yolo detect predict model=/home1/danny472/Underwater/SpikeYOLO/runs/UIIS10K/uw-final/train/weights/best.pt source='/home1/danny472/Underwater/dataset/UIIS10K/images/multiclass_test/test_0002.jpg'
